319ff3941b4546385388f831b92a01f4d4388974
Minseo Han <kuala2038@gmail.com>
2025-04-29 15:28:54 +0900

    Added simple daily problem.

---
+++ 2025/04/20250429/1402.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t, a, b;
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> t;
+
+    while(t--)
+    {
+        cin >> a >> b;
+
+        cout << "yes\n";
+    }
+
+    return 0;
+}
